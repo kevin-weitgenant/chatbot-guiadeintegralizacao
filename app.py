cbd8391ecb0401d34029348934a83d517378c2a9
kevin-weitgenant <kevin.weitgenant@gmail.com>
2021-11-05 13:37:07 -0300

    só uma resposta

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ def get_bot_response():
     userText = request.args.get('msg')
     result = orquestrador2.orquestrador2(userText)
 
-    data = {
-        'respostas': [result[x]["text"][0:1000] for x in range (len(result)) if x< 3]    #retorna até 3 respostas, 
+    #data = {
+        #'respostas': [result[x]["text"][0:1000] for x in range (len(result)) if x< 3]    #retorna até 3 respostas, 
                                                                                          #limitadas por até 1000 caracteres
-    }
+    #}
 
-    json_object = json.dumps(data, indent = 4) 
-    return data
+    #json_object = json.dumps(data, indent = 4) 
+    return result[0]["text"][0:1000]
 
 
 if __name__ == "__main__":
