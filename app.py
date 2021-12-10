fc8b36b3966e777373256e3fb91a47b35339521a
kevin-weitgenant <kevin.weitgenant@gmail.com>
2021-12-10 20:40:25 -0300

    hide button bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request
-import orquestrador 
-import orquestrador2
-import json
+from flask import Flask, render_template, request, jsonify
+import orquestrador
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -13,16 +12,12 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    result = orquestrador2.orquestrador2(userText)
+    result = orquestrador.askDiscovery(userText)
 
-    #data = {
-        #'respostas': [result[x]["text"][0:1000] for x in range (len(result)) if x< 3]    #retorna até 3 respostas, 
+    
+    answers = [result[x]["text"] for x in range (len(result)) if x< 3]           #retorna até 3 respostas, 
                                                                                          #limitadas por até 1000 caracteres
-    #}
-
-    #json_object = json.dumps(data, indent = 4) 
-    return result[0]["text"][0:1000]
-
+    return jsonify(answers)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
