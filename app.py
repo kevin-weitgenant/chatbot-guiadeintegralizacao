815eee17c939c551fbb7056a642bcb95c627cf8c
kevin-weitgenant <kevin.weitgenant@gmail.com>
2021-12-14 14:26:49 -0300

    procfile and app.run'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import orquestrador
-
+import os
 app = Flask(__name__)
 
 
@@ -20,4 +20,5 @@ def get_bot_response():
     return jsonify(answers)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port= port)
